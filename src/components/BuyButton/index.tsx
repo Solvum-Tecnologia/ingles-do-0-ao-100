ac521654cd39b3377d222b33a79259d12031a718
Mardeson Pereira <maxpb777@gmail.com>
2022-03-07 13:47:45 -0300

    feat: active curso

--- src/components/BuyButton/index.tsx
+++ src/components/BuyButton/index.tsx
@@ -5,14 +5,14 @@ import { Container, Button } from './styles';
 type Props = AnchorHTMLAttributes<HTMLAnchorElement>;
 
 const BuyButton: React.FC<Props> = ({ ...rest }) => {
-  const active = false;
+  const active = true;
 
   return (
     <Container>
       <Button
         href={
           active
-            ? 'https://payment.hotmart.com/K39846527P?checkoutMode=10'
+            ? 'https://pay.hotmart.com/K39846527P?checkoutMode=10'
             : 'https://forms.gle/TsaK6eL6HWtcmMKS6'
         }
         data-aos="zoom-in"
--- src/db/topics.ts
+++ src/db/topics.ts
@@ -1,53 +1,33 @@
 const topics = [
   {
-    id: 1,
-    title: '98 aulas oficiais',
-    value: null,
-  },
-  {
-    id: 2,
-    title: 'Dezenas de aulas extras para fixação',
-    value: null,
-  },
-  {
-    id: 3,
-    title: 'Aulas de conversação semanais',
-    value: null,
-  },
-  {
-    id: 4,
-    title: 'Plantão de dúvidas',
-    value: null,
-  },
-  {
-    id: 5,
+    id: '073294b5-819b-499c-b889-50641758b179',
     title: 'Acesso vitalício',
-    value: 257.0,
+    value: 257,
   },
   {
-    id: 6,
-    title: 'Suporte de pronúncia no WhatsApp',
-    value: 327.0,
+    id: 'c224e67d-bc0e-48d8-8183-43b18dcaa9e2',
+    title: 'Grupo de conversação',
+    value: null,
   },
   {
-    id: 7,
+    id: '9a7385fa-e722-42fd-ad14-904d67145719',
+    title: 'Aulas ao vivo semanais',
+    value: null,
+  },
+  {
+    id: '95e72880-a678-4124-a37a-b9c3e6ed2ca9',
     title: 'E-book English for travel',
     value: 37.9,
   },
   {
-    id: 8,
-    title: '+ 365 vídeos bônus',
-    value: 447.0,
-  },
-  {
-    id: 9,
-    title: 'Suporte pedagógico',
+    id: '73b36b9f-b619-40e0-9492-1db33196f4b9',
+    title: 'Suporte 7 dias por semana',
     value: null,
   },
   {
-    id: 10,
-    title: 'Grupo de conversação',
-    value: null,
+    id: 'f4a0b9ea-d225-4593-a509-e9ddd3d3b5da',
+    title: '+365 vídeos bônus',
+    value: 447,
   },
   {
     id: 11,
@@ -62,7 +42,7 @@ const topics = [
   {
     id: 13,
     title: 'Curso completo',
-    value: 1997.0,
+    value: 1200.0,
   },
 ];
 
